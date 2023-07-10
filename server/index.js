d8eeaa8d787c02a863187b4464eac277dbed4999
Kartik Nikam <krt1993@outlook.com>
2023-07-10 18:11:16 +0530

    reference changed in sercer/index.js file

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.get("/", async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(render, () => console.log("Server started on port https://imagenerator-cyuk.onrender.com"));
+    app.listen(8080, () => console.log("Server started on port https://imagenerator-cyuk.onrender.com"));
   } catch (error) {
     console.log(error);
   }
