2bf77f99c00b1176067ad191cd0fba91ed122b2a
Kartik Nikam <krt1993@outlook.com>
2023-07-10 18:03:09 +0530

    render site url

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -31,7 +31,7 @@ const CreatePost = () => {
             try {
                 setGeneratingImg(true);
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/dalle",
+                    "https://imagenerator-cyuk.onrender.com/api/v1/dalle",
                     {
                         method: "POST",
                         headers: {
@@ -65,7 +65,7 @@ const CreatePost = () => {
 
             try {
                 const response = await fetch(
-                    "http://localhost:8080/api/v1/post",
+                    "https://imagenerator-cyuk.onrender.com/api/v1/post",
                     {
                         method: "POST",
                         headers: {
